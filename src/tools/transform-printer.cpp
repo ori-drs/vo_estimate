a4152cf03344da10972d8881188552eac6945b2f
Maurice Fallon <mauricefallon@gmail.com>
2017-08-08 17:49:59 +0100

    adding to ft printer

--- src/tools/transform-printer.cpp
+++ src/tools/transform-printer.cpp
@@ -30,7 +30,7 @@ using namespace std;
 
 struct CommandLineConfig
 {
-  std::string in_log_fname;
+  std::string param_file;
 
 };
 
@@ -59,34 +59,41 @@ class App{
 App::App(boost::shared_ptr<lcm::LCM> &lcm_recv_, boost::shared_ptr<lcm::LCM> &lcm_pub_, const CommandLineConfig& cl_cfg_) :
        lcm_recv_(lcm_recv_), lcm_pub_(lcm_pub_), cl_cfg_(cl_cfg_)
 {
-  do {
-    botparam_ = bot_param_new_from_server(lcm_recv_->getUnderlyingLCM(), 0); // 1 means keep updated, 0 would ignore updates
-  } while (botparam_ == NULL);
-
-  botframes_= bot_frames_get_global(lcm_recv_->getUnderlyingLCM(), botparam_);
+  botparam_ = bot_param_new_from_file(cl_cfg_.param_file.c_str());
+  botframes_ = bot_frames_get_global(lcm_recv_->getUnderlyingLCM(), botparam_);
   botframes_cpp_ = new bot::frames(botframes_);
 
 
 
-
-  Eigen::Affine3d q1;
-  q1.matrix() << 0.0148655429818, -0.999880929698, 0.00414029679422, -0.0216401454975,
+  Eigen::Affine3d imu_cam0_a;
+  imu_cam0_a.matrix() << 0.0148655429818, -0.999880929698, 0.00414029679422, -0.0216401454975,
          0.999557249008, 0.0149672133247, 0.025715529948, -0.064676986768,
         -0.0257744366974, 0.00375618835797, 0.999660727178, 0.00981073058949,
          0.0, 0.0, 0.0, 1.0;
   std::cout << "imu to cam0: \n";
-  printAffine(q1);
+  printAffine(imu_cam0_a);
   std::cout << "\n";
 
 
-  Eigen::Affine3d q2;
-  q2.matrix() << 0.0125552670891, -0.999755099723, 0.0182237714554, -0.0198435579556,
+  Eigen::Affine3d imu_cam1_a;
+  imu_cam1_a.matrix() << 0.0125552670891, -0.999755099723, 0.0182237714554, -0.0198435579556,
          0.999598781151, 0.0130119051815, 0.0251588363115, 0.0453689425024,
         -0.0253898008918, 0.0179005838253, 0.999517347078, 0.00786212447038,
          0.0, 0.0, 0.0, 1.0;
   std::cout << "imu to cam1: \n";
-  printAffine(q2);
+  printAffine(imu_cam1_a);
   std::cout << "\n";
+
+  Eigen::Affine3d cam0_cam1_a = imu_cam1_a.inverse() * imu_cam0_a;
+  std::cout << "cam0 to cam1: \n";
+  printAffine(cam0_cam1_a);
+  std::cout << "\n";
+  
+  std::cout << cam0_cam1_a.rotation() << " rotation\n";
+
+  Eigen::Matrix3d A = cam0_cam1_a.rotation();
+  Eigen::VectorXd B(Eigen::Map<Eigen::VectorXd>(A.data(), A.cols()*A.rows()));
+  std::cout << B.transpose() << " rotation as vector\n";  
 
 
 
@@ -102,6 +109,13 @@ App::App(boost::shared_ptr<lcm::LCM> &lcm_recv_, boost::shared_ptr<lcm::LCM> &lc
   botframes_cpp_->get_trans_with_utime( botframes_ ,  "imu", "CAMERA_RIGHT"  , 0, imu_cam1);
   Eigen::Affine3d imu_cam1_aff = imu_cam1;
   std::cout << imu_cam1.matrix() << " imu_cam1\n";
+
+
+
+  Eigen::Isometry3d cam0_cam1;
+  botframes_cpp_->get_trans_with_utime( botframes_ ,  "CAMERA_LEFT", "CAMERA_RIGHT"  , 0, cam0_cam1);
+  Eigen::Affine3d cam0_cam1_aff = cam0_cam1;
+  std::cout << cam0_cam1.matrix() << " cam0_cam1\n";
 
 
   //Eigen::Matrix3d temp_matrix = temp_frame.matrix();
@@ -139,13 +153,19 @@ void App::printAffine(Eigen::Affine3d q_input){
 
 int main(int argc, char **argv) {
   CommandLineConfig cl_cfg;
-  cl_cfg.in_log_fname = "";
+  std::string param_file = ""; // actual file
+  cl_cfg.param_file = ""; // full path to file
 
   ConciseArgs parser(argc, argv, "simple-fusion");
-  parser.add(cl_cfg.in_log_fname, "L", "in_log_fname", "Process this log file");
+  parser.add(param_file, "c", "param_file", "Process this param file");
   parser.parse();
 
-  std::cout << "Log filename: "              << cl_cfg.in_log_fname         << std::endl;
+  cl_cfg.param_file = std::string(getConfigPath()) +'/' + std::string(param_file);
+  if (param_file.empty()) { // get param from lcm
+    cl_cfg.param_file = "";
+  }
+
+  std::cout << "Log filename: "              << cl_cfg.param_file         << std::endl;
 
   boost::shared_ptr<lcm::LCM> lcm_recv;
   boost::shared_ptr<lcm::LCM> lcm_pub;
@@ -154,5 +174,5 @@ int main(int argc, char **argv) {
 
 
   App app= App(lcm_recv, lcm_pub, cl_cfg);
-  while(0 == lcm_recv->handle());
+  //while(0 == lcm_recv->handle());
 }
